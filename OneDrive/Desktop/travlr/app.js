e5acd4642ffd6168ada3d1c8e4fc2435031087f3
ThomasKller <thomaskller45@gmail.com>
2025-01-19 11:58:26 -0500

    Module2 completed baseline

--- OneDrive/Desktop/travlr/app.js
+++ OneDrive/Desktop/travlr/app.js
@@ -4,15 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./app_server/routes/index');
-var usersRouter = require('./app_server/routes/users');
-var travelRouter = require('./app_server/routes/travel');
-const { handlebars } = require('hbs');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
@@ -23,7 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/travel', travelRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,7 +35,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error.hbs');
+  res.render('error');
 });
 
 module.exports = app;
