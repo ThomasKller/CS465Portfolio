5091e0df403b9b9a44801930668c94e4da79dbab
ThomasKller <thomaskller45@gmail.com>
2025-01-26 11:00:28 -0500

    Module 3 Completed Baseline

--- Downloads/travlr/app.js
+++ Downloads/travlr/app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+const exphbs = require('express-handlebars');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -8,10 +9,25 @@ var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
 const { handlebars } = require('hbs');
+
+
 var app = express();
 
+// Set up Handlebars engine with partials
+app.engine(
+  'hbs',
+  exphbs.engine({
+    extname: '.hbs', // File extension for Handlebars templates
+    defaultLayout: 'layout', // Specify the default layout file
+    layoutsDir: path.join(__dirname, 'app_server', 'views', 'layouts'), // Path to layouts
+    partialsDir: path.join(__dirname, 'app_server', 'views', 'partials'), // Path to partials
+  })
+);
+
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 
 app.set('view engine', 'hbs');
 
@@ -19,11 +35,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,7 +56,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error.hbs');
+  res.render('error');
 });
 
 module.exports = app;
