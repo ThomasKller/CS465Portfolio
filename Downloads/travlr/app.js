e598a083e3b7e3d66f85936e492ccc6034ed57ff
ThomasKller <thomaskller45@gmail.com>
2025-02-09 13:33:00 -0500

    Module 5 completed baseline

--- Downloads/travlr/app.js
+++ Downloads/travlr/app.js
@@ -8,10 +8,15 @@ var logger = require('morgan');
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
+var apiRouter = require('./app_api/routes/index');
+
 const { handlebars } = require('hbs');
 
 
 var app = express();
+
+// Bring in the database
+require ('./app_api/models/db');
 
 // Set up Handlebars engine with partials
 app.engine(
@@ -40,6 +45,7 @@ app.use(cookieParser());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
