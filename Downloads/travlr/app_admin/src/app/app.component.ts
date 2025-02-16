3c9c9fadb5f47bf16776a81fb7276d0bbc082abb
ThomasKller <thomaskller45@gmail.com>
2025-02-16 15:26:00 -0500

    Module 6 Completed Baseline

---
+++ Downloads/travlr/app_admin/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'
+
+import { TripListingComponent } from './trip-listing/trip-listing.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, TripListingComponent, RouterModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  template: '<router-outlet></router-outlet'
+})
+export class AppComponent {
+  title = 'Travlr Getaways Admin!';
+}
